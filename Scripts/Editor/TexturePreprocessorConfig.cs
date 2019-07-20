fa679180c3d2d35b80cd91dad75a942604ad5870
Andre Levi <andrelevi09@gmail.com>
2019-07-20 22:18:50 +0700

    Add more tooltips for TexturePreprocessorConfig

--- Scripts/Editor/TexturePreprocessorConfig.cs
+++ Scripts/Editor/TexturePreprocessorConfig.cs
@@ -22,15 +22,19 @@ namespace AssetPreprocessor.Scripts.Editor
         public List<string> SkipIfCurrentTextureFormatMatchesRegexList = new List<string>();
 
         [Header("Texture Settings")]
+        [Tooltip("Absolute max size allowed for textures. The texture's native size will be used if it is smaller than the max texture size.")]
         public int MaxTextureSize = 4096;
         public bool EnableReadWrite;
         public bool ForceLinear;
+        [Tooltip("Decides how to round when a texture's size is NOT a power of two.")]
         public TextureImporterNPOTScale NPOTScale = TextureImporterNPOTScale.ToNearest;
         [Tooltip("By default each texture's max size will be based upon the texture's native size. Sometimes you might want to use a multiplier (such as 0.5) of that native size.")]
         public float NativeTextureSizeMultiplier = 1f;
 
         [Header("Compression Settings")]
+        [Tooltip("Format used if the texture does NOT have an alpha channel.")]
         public TextureImporterFormat RGBFormat = TextureImporterFormat.Automatic;
+        [Tooltip("Format used if the texture has an alpha channel.")]
         public TextureImporterFormat RGBAFormat = TextureImporterFormat.Automatic;
         public TextureCompressionQuality TextureCompressionQuality = TextureCompressionQuality.Normal;
     }
