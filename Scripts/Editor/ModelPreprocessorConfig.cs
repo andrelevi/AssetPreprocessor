fd55d992b32ae79329c3e57d795f9fca64f6b776
Andre Levi <andrelevi09@gmail.com>
2022-03-15 02:58:50 -0700

    Fix ModelProcessorConfig OdinInspector import not being wrapped in #if preprocessor directive

--- Scripts/Editor/ModelPreprocessorConfig.cs
+++ Scripts/Editor/ModelPreprocessorConfig.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
-using Sirenix.OdinInspector;
 using UnityEditor;
 using UnityEngine;
+
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
 
 namespace AssetPreprocessor.Scripts.Editor
 {
